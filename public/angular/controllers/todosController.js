a4a75ce3583dc7a54b8d0fef3bf5862806a103c5
TomerBu <Tomer.bu@gmail.com>
2017-01-04 14:17:57 +0200

    simplified todos

--- public/angular/controllers/todosController.js
+++ public/angular/controllers/todosController.js
@@ -4,30 +4,22 @@
 
     //A controller is just a function. (The $scope is injected by angular at runtime)
     var todosController = function ($scope) {
-        $scope.todoList = [
+        $scope.todos = [
             { text: 'learn angular', done: true },
             { text: 'build an angular app', done: false }
         ];
 
-        $scope.todoList.addTodo = function () {
-            todoList.todos.push({ text: todoList.todoText, done: false });
-            todoList.todoText = '';
+        $scope.todos.addTodo = function () {
+            $scope.todos.push({ text: todoList.todoText, done: false });
+            $scope.todoText = '';
         };
 
-        $scope.todoList.remaining = function () {
+        $scope.todos.remaining = function () {
             var count = 0;
-            angular.forEach(todoList.todos, function (todo) {
+            angular.forEach($scope.todos, function (todo) {
                 count += todo.done ? 0 : 1;
             });
             return count;
-        };
-
-        $scope.todoList.archive = function () {
-            var oldTodos = todoList.todos;
-            todoList.todos = [];
-            angular.forEach(oldTodos, function (todo) {
-                if (!todo.done) todoList.todos.push(todo);
-            });
         };
     }
     //protection from code minifiers:
