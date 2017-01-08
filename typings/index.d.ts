e3c8d2554a9eb35706d2a4577ccf8ab3d2166c7c
TomerBu <Tomer.bu@gmail.com>
2017-01-08 09:54:27 +0200

    angular-route typings

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,4 +1,5 @@
 /// <reference path="globals/jquery/index.d.ts" />
 /// <reference path="globals/node/index.d.ts" />
+/// <reference path="modules/angular-route/index.d.ts" />
 /// <reference path="modules/angular/index.d.ts" />
 /// <reference path="modules/express/index.d.ts" />
